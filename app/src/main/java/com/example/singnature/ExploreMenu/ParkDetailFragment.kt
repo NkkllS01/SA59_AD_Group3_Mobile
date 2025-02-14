ddd8ab7ace5d2efa8ea448f8e0378e0380c697fe
NkkllS <nkklls01@gmail.com>
2025-02-14 11:21:51 +0800

    park details

--- app/src/main/java/com/example/singnature/ExploreMenu/ParkDetailFragment.kt
+++ app/src/main/java/com/example/singnature/ExploreMenu/ParkDetailFragment.kt
@@ -4,14 +4,17 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
 import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.activityViewModels
+import com.bumptech.glide.Glide
 import com.example.singnature.R
 
 class ParkDetailFragment : Fragment() {
     private val parkViewModel: ParkViewModel by activityViewModels()
 
+    private lateinit var parkImageView: ImageView
     private lateinit var parkNameTextView: TextView
     private lateinit var openingHoursTextView: TextView
     private lateinit var descriptionTextView: TextView
@@ -28,6 +31,7 @@ class ParkDetailFragment : Fragment() {
         super.onViewCreated(view, savedInstanceState)
 
         // Initialize views
+        parkImageView = view.findViewById(R.id.parkImage)
         parkNameTextView = view.findViewById(R.id.parkName)
         descriptionTextView = view.findViewById(R.id.parkDescription)
         openingHoursTextView = view.findViewById(R.id.openingHours)
@@ -38,9 +42,14 @@ class ParkDetailFragment : Fragment() {
         parkViewModel.fetchParkDetail(parkId)
 
         parkViewModel.parkDetail.observe(viewLifecycleOwner) { park ->
-                parkNameTextView.text = park.parkName
-                descriptionTextView.text = park.parkDescription
+            parkNameTextView.text = park.parkName
+            descriptionTextView.text = park.parkDescription
+            openingHoursTextView.text = park.openingHours
 
+            // 加载图片
+            Glide.with(this)
+                .load(park.imageUrl) // 这里是你的图片 URL
+                .into(parkImageView)
         }
     }
 }
